614b23edff1a3682f01a5ac4e2860a3d2053c476
i514097 <tianli.gao@sap.com>
2021-02-13 23:26:22 -0500

    initial commit

--- src/main/java/com/store/demo/service/ImportStockService.java
+++ src/main/java/com/store/demo/service/ImportStockService.java
@@ -2,17 +2,14 @@ package com.store.demo.service;
 
 import com.store.demo.model.DailyPrice;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,7 +17,8 @@ import java.util.stream.Collectors;
 @Slf4j
 public class ImportStockService
 {
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	private static final String DATE_FORMAT_PATTERN = "yyyy-MM-dd";
+
 	private StockRecordService stockRecordService;
 
 	public ImportStockService(final StockRecordService recordService)
@@ -33,7 +31,7 @@ public class ImportStockService
 		try
 		{
 			final List<DailyPrice> lines = Files.lines(Paths.get(ClassLoader.getSystemResource(filename).toURI()))
-					.map(line -> buildFromFileRow(line))
+					.map(line -> buildDailyPrice(line))
 					.collect(Collectors.toList());
 
 			stockRecordService.save(lines);
@@ -48,33 +46,23 @@ public class ImportStockService
 		}
 	}
 
-	private DailyPrice buildFromFileRow(final String fileRow)
+	private DailyPrice buildDailyPrice(final String fileRow)
 	{
 		final String[] tokens = fileRow.split("\\|");
 		assert tokens.length == 7;
 
-		try
-		{
-			final Date recordedDate = dateFormat.parse(tokens[1]);
-			final Calendar cal = DateUtils.toCalendar(recordedDate);
+		final LocalDate date = LocalDate.from(DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN).parse(tokens[1]));
 
-			return DailyPrice.builder()
-					.symbol(tokens[0])
-					.year(cal.get(Calendar.YEAR))
-					.month(cal.get(Calendar.MONTH) + 1)
-					.day(cal.get(Calendar.DAY_OF_MONTH))
-					.value(Double.parseDouble(tokens[2]))
-					.previousValue(Double.parseDouble(tokens[3]))
-					.valueChange(Double.parseDouble(tokens[4]))
-					.percentChange(Double.parseDouble(tokens[5]))
-					.shareVolume(Integer.parseInt(tokens[6]))
-					.build();
-		}
-		catch (ParseException exception)
-		{
-			log.warn("Error occurred while parsing the record.", exception);
-
-		}
-		return null;
+		return DailyPrice.builder()
+				.symbol(tokens[0])
+				.year(date.getYear())
+				.month(date.getMonthValue())
+				.day(date.getDayOfMonth())
+				.value(Double.parseDouble(tokens[2]))
+				.previousValue(Double.parseDouble(tokens[3]))
+				.valueChange(Double.parseDouble(tokens[4]))
+				.percentChange(Double.parseDouble(tokens[5]))
+				.shareVolume(Integer.parseInt(tokens[6]))
+				.build();
 	}
 }
